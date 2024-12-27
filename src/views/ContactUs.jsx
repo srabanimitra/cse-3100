fb406570777153000b1e872c150f1f8905f052d1
srabanimitra <srabanimitra2222@gmail.com>
2024-12-27 11:36:19 +0600

    navbar

--- src/views/ContactUs.jsx
+++ src/views/ContactUs.jsx
@@ -8,7 +8,7 @@ export default function ContactUs() {
   const handleSubmit = (event) => {
     event.preventDefault();
     alert(`Thank you for contacting us, ${name}!`);
- 
+
     // Reset form fields
     setName('');
     setPhone('');
@@ -24,24 +24,24 @@ export default function ContactUs() {
         Maecenas leo lorem, tincidunt et, consectetur adipiscing.
       </p>
 
-      <form onSubmit={handleSubmit} className="mt-4">
+      <form onSubmit={handleSubmit} className="mt-4  w-60 h-10">
         <div className="mb-3">
           <input 
             type="text" 
             placeholder="Name" 
             value={name} 
             onChange={(e) => setName(e.target.value)} 
-            className="form-control" 
+            className="form-control text-center bg-pink-100 placeholder-pink-400 py-1 px-2 rounded-md w-60 h-10" 
             required 
           />
         </div>
-        <div className="mb-3">
+        <div className="mb-3  w-60 h-10">
           <input 
             type="tel" 
             placeholder="Phone" 
             value={phone} 
             onChange={(e) => setPhone(e.target.value)} 
-            className="form-control" 
+            className="form-control text-center bg-pink-100 placeholder-pink-400 py-1 px-2 rounded-md w-60 h-10" 
             required 
           />
         </div>
@@ -51,11 +51,11 @@ export default function ContactUs() {
             placeholder="Email" 
             value={email} 
             onChange={(e) => setEmail(e.target.value)} 
-            className="form-control" 
+            className="form-control text-center bg-pink-100 placeholder-pink-400 py-1 px-2 rounded-md w-60 h-10" 
             required 
           />
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary bg-pink-200 rounded-xl">Submit</button>
       </form>
     </section>
   );
